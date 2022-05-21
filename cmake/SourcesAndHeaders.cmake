28abd9bf710c0728575b4198b63b995aa98fb989
yunwei37 <1067852565@qq.com>
2022-05-21 12:29:34 -0700

    add from template

---
+++ cmake/SourcesAndHeaders.cmake
@@ -0,0 +1,16 @@
+set(sources
+    src/tmp.cpp
+)
+
+set(exe_sources
+		src/main.cpp
+		${sources}
+)
+
+set(headers
+    include/project/tmp.hpp
+)
+
+set(test_sources
+  src/tmp_test.cpp
+)
---
+++ src/tmp.cpp
@@ -0,0 +1,6 @@
+#include "eunomia/tmp.hpp"
+
+int tmp::add(int a, int b)
+{
+  return a + b;
+}
