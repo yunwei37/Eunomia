fbb226ecb44c67fed28dde1b40469b1f456bc250
yunwei37 <1067852565@qq.com>
2022-08-18 06:40:09 -0700

    reorg client example

--- bpftools/client-template/client.bpf.c
+++ bpftools/client-template/client.bpf.c
@@ -14,19 +14,37 @@ struct {
 	__uint(max_entries, 256 * 1024);
 } rb SEC(".maps");
 
-SEC("tracepoint/syscalls/sys_enter_open")
-int handle_exec(struct trace_event_raw_sys_enter* ctx)
+SEC("tp/sched/sched_process_exec")
+int handle_exec(struct trace_event_raw_sched_process_exec *ctx)
 {
-	u64 id = bpf_get_current_pid_tgid();
-
-	u32 pid = id;
+	struct task_struct *task;
+	unsigned fname_off;
 	struct event *e;
+	pid_t pid;
+	u64 ts;
+
+	/* remember time exec() was executed for this PID */
+	pid = bpf_get_current_pid_tgid() >> 32;
+	// ts = bpf_ktime_get_ns();
+	// bpf_map_update_elem(&exec_start, &pid, &ts, BPF_ANY);
+
 	/* reserve sample from BPF ringbuf */
 	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
 	if (!e)
 		return 0;
+
+	/* fill out the sample with data */
+	task = (struct task_struct *)bpf_get_current_task();
+
+	e->exit_event = false;
 	e->pid = pid;
-	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx->args[0]);
+	e->duration_ns = 1;
+	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
+	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+
+	fname_off = ctx->__data_loc_filename & 0xFFFF;
+	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx + fname_off);
+
 	/* successfully submit it to user-space for post-processing */
 	bpf_ringbuf_submit(e, 0);
 	return 0;
